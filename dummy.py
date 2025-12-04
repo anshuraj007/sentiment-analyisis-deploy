f8744bbc21d2bb92ed64dcd58a517052b99e2b39
Anshu Raj <22f3000757@ds.study.iitm.ac.in>
2025-12-04 20:00:34 +0530

    Xyz

--- dummy.py
+++ dummy.py
@@ -1,1 +1,13 @@
-print("All in here")
+import streamlit as st
+
+# Title
+st.title("Simple Addition App")
+
+# Inputs
+num1 = st.number_input("Enter first number", value=0.0)
+num2 = st.number_input("Enter second number", value=0.0)
+
+# Button
+if st.button("Add"):
+    result = num1 + num2
+    st.success(f"Result: {result}")
