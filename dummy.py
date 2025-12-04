8beaac5cc99ab468761154b1e7914c515848580b
Anshu Raj <22f3000757@ds.study.iitm.ac.in>
2025-12-04 20:03:59 +0530

    swdqefwr

--- dummy.py
+++ dummy.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 # Title
-st.title("Simple Addition App")
+st.title("Simple Subtraction App")
 
 # Inputs
 num1 = st.number_input("Enter first number", value=0.0)
 num2 = st.number_input("Enter second number", value=0.0)
 
 # Button
-if st.button("Add"):
-    result = num1 + num2
+if st.button("Sub"):
+    result = abs(num1 - num2)
     st.success(f"Result: {result}")
